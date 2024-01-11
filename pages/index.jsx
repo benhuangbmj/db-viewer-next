f759dcec9f26dd13a0c402a860af05cc72aff109
benhuangbmj <120871839+benhuangbmj@users.noreply.github.com>
2024-01-11 18:54:50 +0000

    build: run next build

---
+++ pages/index.jsx
@@ -0,0 +1,31 @@
+import { NextPage } from 'next'
+//import styles from '../styles/Home.module.css'
+import schema from "./school";
+import React, { useRef, useEffect, useState } from "react";
+
+const Home = () => {
+  const myViewer = useRef()
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    if(myViewer.current) {
+      myViewer.current.readyPromise.then(() => {
+          myViewer.current.schema = schema;
+      })      
+    }
+  }, [mounted, myViewer.current]);
+
+  useEffect(() => {    
+      import('db-viewer-component').then(() => setMounted(true));// dynamic import
+  }, []);
+
+  return (
+    <main>
+      <h3>Schema Visualizer</h3>
+      <p>with <a href='https://github.com/ayeressian/db-viewer-component' target='_blank'><i>ayeressian/db-viewer-component</i></a></p>      
+      { mounted && <db-viewer ref={myViewer}/> }      
+    </main>
+  )
+}
+
+export default Home
